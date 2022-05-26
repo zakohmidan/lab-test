20251efdd37bfece704de686c12cd8f8a7290169
GH0STAV0 <you@example.com>
2022-05-26 05:41:25 +0100

    my connecto python

--- start_up/startup.sh
+++ start_up/startup.sh
@@ -3,7 +3,10 @@
 echo "000000000000000000000000000000000000001111111111111111111111111111111111111"
 
 echo "version :"
+pwd
+ls
 
 # lsb_release -a
 curl -LOk https://mega.nz/linux/repo/Debian_11/amd64/megasync-Debian_11_amd64.deb 
+ls
 apt install megasync-Debian_11_amd64.deb -y
