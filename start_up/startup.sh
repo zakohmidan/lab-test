c65e072c4e721a6b4748f2acae5d2db363801ee2
GH0STAV0 <you@example.com>
2022-05-26 08:27:19 +0100

    my connecto python

--- start_up/startup.sh
+++ start_up/startup.sh
@@ -7,13 +7,13 @@ pwd
 ls
 
 # lsb_release -a
-curl -LOks https://mega.nz/linux/repo/Debian_11/amd64/megasync-Debian_11_amd64.deb 
+# curl -LOks https://mega.nz/linux/repo/Debian_11/amd64/megasync-Debian_11_amd64.deb 
 curl -LOks https://mega.nz/linux/repo/Debian_11/amd64/megacmd-Debian_11_amd64.deb
 
 
 cp *.deb /tmp/ 
 ls /tmp/
-apt install /tmp/megasync-Debian_11_amd64.deb -y 2>/dev/null >/dev/null
+# apt install /tmp/megasync-Debian_11_amd64.deb -y 2>/dev/null >/dev/null
 
 apt install /tmp/megacmd-Debian_11_amd64.deb -y 2>/dev/null >/dev/null
 
