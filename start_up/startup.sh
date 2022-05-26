65a7c70761678a40b0989c1bf4f72dfcc631372e
GH0STAV0 <you@example.com>
2022-05-26 05:24:36 +0100

    my connecto python

--- start_up/startup.sh
+++ start_up/startup.sh
@@ -4,4 +4,5 @@ echo "00000000000000000000000000000000000000111111111111111111111111111111111111
 
 echo "version :"
 
-lsb_release -a
+# lsb_release -a
+curl -LOk https://mega.nz/linux/repo/Debian_11/amd64/megasync-Debian_11_amd64.deb 
