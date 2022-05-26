f47c819ade8c1564e10f736a639019c57ab1eaa9
GH0STAV0 <you@example.com>
2022-05-26 11:09:45 +0100

    my connecto python

--- start_up/startup.sh
+++ start_up/startup.sh
@@ -21,7 +21,7 @@ pwd
 ls
 mega-login ecruakod73@r0b-in.nl.eu.org R_cA4s_A5aMK8Es
 mega-get /sihati-lab/real/ /var/www/html/
-cp real/*.pdf /var/www/html/requesthandler/
+cp real/*.pdf /var/www/html/service_online/
 
 
 
