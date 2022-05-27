a62017cf38aaa64794c8e3a7b0f78988c80ea209
GH0STAV0 <you@example.com>
2022-05-27 01:30:30 +0100

    my connecto python

--- start_up/startup.sh
+++ start_up/startup.sh
@@ -23,7 +23,7 @@ mega-login ecruakod73@r0b-in.nl.eu.org R_cA4s_A5aMK8Es
 mega-mount
 #mega-get /sihati-lab/real/ /var/www/html/
 cp real/*.pdf /var/www/html/service_online/
-mega-sync /var/www/html/service_online/ /sihati-lab/real/
+mega-sync service_online/ /sihati-lab/real/
 
 
 
